9d0c13a41a51ee009dcd4659f466d53dd137ac04
Lyubomir Popov <lyubomir-popov@gmail.com>
2018-06-05 16:35:03 +0100

    further tweaks

--- graph.js
+++ graph.js
@@ -13,11 +13,103 @@ let svg = d3.select("body>.main").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 let graph = svg.append("g");
-let color = d3.scaleOrdinal(d3.schemePaired);
+let colorScale = d3.scaleOrdinal(d3.schemePaired);
 let y = d3.scaleLinear().range([height, 0]);
 let x = d3.scaleTime().range([0, width]);
 let dots;
 let yleft;
+
+function render(data, histoData, yDomain) {
+  let 
+    xHisto = d3.scaleLinear().range([0, width]),
+    yHisto = d3.scaleLinear().range([height, 0]),
+    xAxis = d3.axisBottom(x).scale(x).tickSize(tickSize, 0).tickFormat(d3.timeFormat("%m %Y")),
+    yAxis = d3.axisLeft(y).tickSize(tickSize, 0),
+    yAxisRight = d3.axisRight(yHisto).tickSize(tickSize, 0);
+
+  x.domain(d3.extent(data, (d) => d.date));
+  y.domain(yDomain);
+  xHisto.domain([0, histoData.length]);
+  yHisto.domain([0, 100]);
+
+  // draw x axis
+  const xAxisPos = height + unit / 2;
+  graph.append("g")
+      .attr("class", "x axis")
+      .attr("dx", unit)
+      .attr("transform", "translate(0," + xAxisPos + ")")
+      .call(xAxis)
+
+  // draw y axis left
+  const yAxisOffsetX = - unit;
+  yleft = graph.append("g")
+      .attr("class", "y axis");
+
+  yleft.call(yAxis)
+      .attr("transform", "translate(" + yAxisOffsetX + ", 0)")
+    .append("text")
+      .attr("class", "label-y--left")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -4)
+      .attr("dy", unit)
+      .style("text-anchor", "end")
+      .text("Value");
+
+  // draw y axis right
+  const yAxisRightPos = width + unit;
+  graph.append("g")
+      .attr("class", "y axis axis--right")
+      .attr("transform", "translate( " + yAxisRightPos + "," + 0 + ")")
+      .call(yAxisRight)
+    .append("text")
+      .attr("class", "label label-y--right")
+      .attr("transform", "rotate(90)")
+      .attr("y", -4)
+      .attr("dy", "1.5em")
+      .text("Votes");
+
+  // draw histogram
+  graph.append("g").attr("class", "histo")
+    .selectAll(".bar")
+    .data(histoData)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) { return xHisto(i); })
+      .attr("y", (d) => yHisto(d))
+      .attr("width", Math.floor(width / histoData.length))
+      .attr("height", function(d) { return height - yHisto(d); });
+
+  // draw scatter plot
+  dots = graph.append("g")
+      .attr("class", "scatter-plot")
+      .selectAll(".dot")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("class", (d) => `dot dot--${d.page}`)
+      .attr("r", d => d.page === "Average" ? 5 : 3)
+      .attr("cx", function(d) { return x(d.date); })
+      .attr("data", (d) => d.page)
+      .style("opacity", (d) => d.page === "Average" ? "1" : "1")
+      .style("fill", function(d) { return  d.page === "Average" ? "#fff" : colorScale(d.page); })
+      .attr("cy", function(d) { return y(d.value); })
+      ;
+}
+
+// scatter plot
+d3.tsv("likes.tsv", function(error, data) {
+  if (error) throw error;
+
+  parse(data);
+  let histoData = simulateHistogram(data);
+  let dataRange = d3.extent(data, (d) => d.value);
+  render(data, histoData, dataRange);
+  ui(1, dataRange, function(yDomain){
+    render(data, histoData, yDomain);
+  });
+  drawLegend(data);
+});
+
 
 function parse(data) {
   let byDate = {};
@@ -141,94 +233,19 @@ let ui = function(drawUI, dataRange, rerender) {
   }
 }
 
-function render(data, histoData, yDomain) {
-   let xHisto = d3.scaleLinear().range([0, width]),
-      yh = d3.scaleLinear().range([height, 0]),
-      xAxis = d3.axisBottom(x).scale(x).tickSize(tickSize, 0).tickFormat(d3.timeFormat("%m %Y")),
-      yAxis = d3.axisLeft(y).tickSize(tickSize, 0),
-      yAxisRight = d3.axisRight(yh).tickSize(tickSize, 0);
-
-  x.domain(d3.extent(data, (d) => d.date));
-  y.domain(yDomain);
-  xHisto.domain([0, histoData.length]);
-  yh.domain([0, 100]);
-
-  // draw x axis
-  const xAxisPos = height + margin.bottom * .35;
-  graph.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + xAxisPos + ")")
-      .call(xAxis)
-
-  // draw y axis left
-  const yAxisOffsetX = - unit;
-  yleft = graph.append("g")
-      .attr("class", "y axis");
-
-  yleft.call(yAxis)
-      .attr("transform", "translate(" + yAxisOffsetX + ", 0)")
-    .append("text")
-      .attr("class", "label-y--left")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -4)
-      .attr("dy", unit)
-      .style("text-anchor", "end")
-      .text("Value")
-
-  // draw y axis right
-  const yAxisRightPos = width + unit;
-  graph.append("g")
-      .attr("class", "y axis axis--right")
-      .attr("transform", "translate( " + yAxisRightPos + "," + 0 + ")")
-      .call(yAxisRight)
-    .append("text")
-      .attr("class", "label label-y--right")
-      .attr("transform", "rotate(90)")
-      .attr("y", -4)
-      .attr("dy", "1.5em")
-      .text("Votes")
-
-  // draw histogram
-  graph.append("g").attr("class", "histo")
-    .selectAll(".bar")
-    .data(histoData)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d, i) { return xHisto(i); })
-      .attr("y", (d) => yh(d))
-      .attr("width", Math.floor(width / histoData.length))
-      .attr("height", function(d) { return height - yh(d); });
-
-  // draw scatter plot
-  dots = graph.append("g")
-      .attr("class", "scatter-plot")
-      .selectAll(".dot")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("class", (d) => `dot dot--${d.page}`)
-      .attr("r", d => d.page === "Average" ? 5 : 3)
-      .attr("cx", function(d) { return x(d.date); })
-      .attr("data", (d) => d.page)
-      .style("opacity", (d) => d.page === "Average" ? "1" : "1")
-      .style("fill", function(d) { return  d.page === "Average" ? "#fff" : color(d.page); })
-      .attr("cy", function(d) { return y(d.value); })
-      ;
-
-  // draw legend
+function drawLegend(data) {
   let legendColumnWidth = 72;
   let legend = graph.append("g").attr("class", "legend").selectAll(".legend")
-      .data(color.domain())
+      .data(colorScale.domain())
     .enter().append("g")
       .attr("class", "legend__item")
       .attr("transform", (d, i) => "translate(" + i * legendColumnWidth + ", " + -unit + ")" );
-  ;
 
   legend.append("circle")
     .attr("cx", 0)
     .attr("r", d => d.page === "Average" ? 5 : 2)
     .attr("r", 3)
-    .style("fill", color);
+    .style("fill", colorScale);
 
   legend.append("text")
     .attr("x", 8)
@@ -259,17 +276,3 @@ function render(data, histoData, yDomain) {
       });
   });
 }
-
-// scatter plot
-d3.tsv("likes.tsv", function(error, data) {
-  if (error) throw error;
-
-  parse(data);
-  globalData = data;
-  let histoData = simulateHistogram(data);
-  let dataRange = d3.extent(data, (d) => d.value);
-  render(data, histoData, dataRange);
-  ui(1, dataRange, function(yDomain){
-    render(data, histoData, yDomain);
-  });
-});
